aab1d564b77b1c5db99d784057399dd55bda945e
Abigail  Mabe <amabe@Abigails-MacBook-Pro.local>
2022-11-10 12:56:09 -0500

    Initial deploy React app to S3

---
+++ terraform/s3.tf
@@ -0,0 +1,45 @@
+# Create the bucket
+resource "aws_s3_bucket" "app_bucket" {
+  bucket        = var.bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "app_bucket_acl" {
+  bucket = aws_s3_bucket.app_bucket.id
+  acl    = "public-read"
+}
+
+# Setup the website configuration
+resource "aws_s3_bucket_website_configuration" "app_bucket_web_config" {
+  bucket = aws_s3_bucket.app_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+# Add the website files
+module "website_files" {
+  source   = "hashicorp/dir/template"
+  base_dir = var.website_root
+}
+
+resource "aws_s3_object" "static_files" {
+  for_each     = module.website_files.files
+  acl          = "public-read"
+  bucket       = var.bucket_name
+  key          = each.key
+  content_type = each.value.content_type
+  source       = each.value.source_path
+  content      = each.value.content
+  etag         = each.value.digests.md5
+}
+
+# Output the endpoint
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.app_bucket_web_config.website_endpoint
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "bucket_name" {
+  type        = string
+  description = "Name of the bucket to store the React app."
+  default     = "aws-hosted-react"
+}
+
+variable "website_root" {
+  type        = string
+  description = "Path to the root of website content"
+  default     = "../app/build"
+}
