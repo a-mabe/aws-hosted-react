4629b19aea80fc5b73902b8719165c7d11e73dd1
a-mabe <mabe.abby.a@gmail.com>
2022-11-14 15:34:53 -0500

    Use artifact in apply stage

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,5 +7,5 @@ variable "bucket_name" {
 variable "website_root" {
   type        = string
   description = "Path to the root of website content"
-  default     = "../app/build"
+  default     = "../react-build"
 }
