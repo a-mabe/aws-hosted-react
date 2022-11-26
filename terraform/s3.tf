30410fd8e349078e978cb04669269b4b821dd24e
a-mabe <mabe.abby.a@gmail.com>
2022-11-26 12:34:34 -0500

    Add public access block

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,23 +4,15 @@ resource "aws_s3_bucket" "app_bucket" {
   force_destroy = true
 }
 
-# resource "aws_s3_bucket_acl" "app_bucket_acl" {
-#   bucket = aws_s3_bucket.app_bucket.id
-#   acl    = "public-read"
-# }
+# Block public access
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = aws_s3_bucket.app_bucket.id
 
-# Setup the website configuration
-# resource "aws_s3_bucket_website_configuration" "app_bucket_web_config" {
-#   bucket = aws_s3_bucket.app_bucket.bucket
-
-#   index_document {
-#     suffix = "index.html"
-#   }
-
-#   error_document {
-#     key = "index.html"
-#   }
-# }
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
 # Add the website files
 module "website_files" {
