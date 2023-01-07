c154a6525e478d0552b1e263df390b922f693b5a
a-mabe <mabe.abby.a@gmail.com>
2023-01-06 20:16:37 -0500

    Remove policy tag

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -13,8 +13,4 @@ data "aws_iam_policy_document" "aws_hosted_react_cloudfront_policy_doc" {
 resource "aws_s3_bucket_policy" "aws_hosted_react_cloudfront_policy" {
   bucket = aws_s3_bucket.app_bucket.id
   policy = data.aws_iam_policy_document.aws_hosted_react_cloudfront_policy_doc.json
-
-  tags = {
-    Project = "aws-hosted-react"
-  }
 }
